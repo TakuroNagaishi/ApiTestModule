f3096c03d3ced8418c2441f01778fad986cdb4e4
TakuroNagaishi <takuro.nagaishi@gmail.com>
2022-08-11 13:11:57 +0900

    commit

--- Scripts/format_json.php
+++ Scripts/format_json.php
@@ -1,6 +1,6 @@
 <?php
 
-
 $file_path = $argv[1];
 $json_str = file_get_contents($file_path);
-file_put_contents($file_path, json_encode(json_decode($json_str), JSON_PRETTY_PRINT));
+// file_put_contents($file_path, json_encode(json_decode($json_str), JSON_PRETTY_PRINT)); //For evidence
+file_put_contents($file_path, json_encode(json_decode($json_str), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)); //To test
--- main.sh
+++ main.sh
@@ -2,8 +2,17 @@
 
 cd `dirname $0`
 
+trap 'exit' INT
+
+# When a project name is not entered.
+if [ $# = 0 ]; then
+  echo "Project name is required."
+  exit 0
+fi
+
 PROJECT_DIRECTORY=Projects/$1/
 
+# When the project is not existed.
 if [ ! -e $PROJECT_DIRECTORY ]; then
   echo "No such project. Please create test project."
   exit 0
@@ -12,7 +21,7 @@ fi
 # Read settings
 source $PROJECT_DIRECTORY/settings.txt
 
-# Initialize response directory.
+# Initialize the response directory.
 rm -r $PROJECT_DIRECTORY/Responses/
 mkdir $PROJECT_DIRECTORY/Responses/
 
@@ -22,7 +31,9 @@ for request in `ls $PROJECT_DIRECTORY/Requests/`;
     RESPONSE_FILE=$PROJECT_DIRECTORY/Responses/$request
 
     case "$METHOD" in
-      POST ) curl -s -X POST -H $HEADER -d @$PROJECT_DIRECTORY/Requests/$request $URL > $RESPONSE_FILE;;
+      POST )
+        curl -s -X POST -H $HEADER -d @$PROJECT_DIRECTORY/Requests/$request $URL > $RESPONSE_FILE;
+        echo "Executing $request.";;
       * ) echo "No such method: $METHOD" ;;
     esac
   done;
@@ -32,3 +43,5 @@ for response in `ls $PROJECT_DIRECTORY/Responses/`;
   do
     php Scripts/format_json.php $PROJECT_DIRECTORY/Responses/$response;
   done;
+
+echo -e "\e[32mTest completed successfully.\e[m"
